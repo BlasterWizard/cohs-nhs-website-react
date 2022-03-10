72e707fc254564a79f77818d657065a0f1fff73b
Justin W <51015074+BlasterWizard@users.noreply.github.com>
2022-03-10 09:55:16 -0800

    admin attendance display events amount func

--- src/admin/admin-attendance/AdminAttendance.tsx
+++ src/admin/admin-attendance/AdminAttendance.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import { useEffect } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Button, Form, Overlay, Table, Tooltip } from "react-bootstrap";
 import {Event, Student } from "../../App";
 import AdminPagination, {
   AdminPaginationKeys,
@@ -47,9 +47,10 @@ const AdminAttendance: React.FC<AdminAttendanceProps> = ({
   const [studentList, setStudentList] = useState<Student[]>([]);
   const [changes, setChanges] = useState<SheetChange[]>([]);
   const [show, setShow] = useState(false);
-  const [showEventsAmount, setShowEventsAmount] = useState<number>(6);
+  const [displayEventsAmount, setDisplayEventsAmount] = useState<number>(6);
+  const [preDisplayEventsAmount, setPreDisplayEventsAmount] = useState<number>(6);
   const [startShowEventIndex, setStartShowEventIndex] = useState<number>(0);
-  const [endShowEventIndex, setEndShowEventIndex] = useState<number>(showEventsAmount);
+  const [endShowEventIndex, setEndShowEventIndex] = useState<number>(displayEventsAmount);
   const gradeSelectionOptions= [
     {value: GradeType.Senior, label: "Seniors"},
     {value: GradeType.Junior, label: "Juniors"}
@@ -58,15 +59,18 @@ const AdminAttendance: React.FC<AdminAttendanceProps> = ({
     value: GradeType.Senior, 
     label: "Seniors"
   });
+  const [showAdminAttendanceSettings, setShowAdminAttendanceSettings] = useState(false);
+  const [showDisplayEventsAmountError, setShowDisplayEventsAmountError] = useState(false);
 
   useEffect(() => {
     setStudentList(students.sort((a, b) => a.name.localeCompare(b.name)));
-  }, [students, events]);
+  }, [students, events, displayEventsAmount]);
 
   const handleShow = () => setShow(true);
   const handleClose = () => {
     setShow(false);
   };
+  const settingsTarget = useRef(null);
 
   const gradeSelectionHandler = (e: any) => {
     switch(e.value) {
@@ -80,8 +84,8 @@ const AdminAttendance: React.FC<AdminAttendanceProps> = ({
   }
 
   const reverseEventIndicies = () => {
-    if ((startShowEventIndex - showEventsAmount - 1) >= 0) {
-      setStartShowEventIndex(startShowEventIndex - showEventsAmount - 1);
+    if ((startShowEventIndex - displayEventsAmount - 1) >= 0) {
+      setStartShowEventIndex(startShowEventIndex - displayEventsAmount - 1);
     } else {
       setStartShowEventIndex(0);
     }
@@ -97,11 +101,21 @@ const AdminAttendance: React.FC<AdminAttendanceProps> = ({
       setStartShowEventIndex(endShowEventIndex);
     }
 
-    if ((endShowEventIndex + showEventsAmount) >= events.length) {
+    if ((endShowEventIndex + displayEventsAmount) >= events.length) {
       setEndShowEventIndex(events.length);
     } else {
-      setEndShowEventIndex(endShowEventIndex + showEventsAmount);
+      setEndShowEventIndex(endShowEventIndex + displayEventsAmount);
     } 
+  }
+
+  const preDisplayEventAmountsHandler = (e: any) => {
+    setPreDisplayEventsAmount(e.target.value);
+    if (e.target.value <= events.length && e.target.value > 0) {
+      setEndShowEventIndex(startShowEventIndex + e.target.value);
+      setShowDisplayEventsAmountError(false);
+    } else {
+      setShowDisplayEventsAmountError(true);
+    }
   }
 
   if (isLoading) {
@@ -122,14 +136,40 @@ const AdminAttendance: React.FC<AdminAttendanceProps> = ({
         
       <div className="bg-white/60 p-2 rounded-2xl flex flex-col items-center mt-3">
         <div className="flex items-center w-full my-3">
-          <button onClick={reverseEventIndicies}><i className="fas fa-chevron-left ml-5 bg-indigo-400 hover:bg-indigo-500 py-2.5 px-3 rounded-full text-white"></i></button>
+          <button disabled={startShowEventIndex === 0} onClick={reverseEventIndicies}>
+            <i className={startShowEventIndex === 0 ? "fas fa-chevron-left ml-5 bg-indigo-300 py-2.5 px-3 rounded-full text-white" : "fas fa-chevron-left ml-5 bg-indigo-400 hover:bg-indigo-500 py-2.5 px-3 rounded-full text-white"}></i>
+          </button>
           <div className="flex-grow"></div>
           <div className="flex space-x-3 items-center">
             <ReactSelect defaultValue={gradeSelection} value={gradeSelection} options={gradeSelectionOptions} onChange={gradeSelectionHandler} className="text-black font-bold w-48 text-center text-xl" closeMenuOnSelect={true}/>
-            <Button className="text-black border-0 text-xl"><i className="fas fa-cog"></i></Button>
+            <Button ref={settingsTarget} onClick={() => setShowAdminAttendanceSettings(!showAdminAttendanceSettings)} className="hover:bg-transparent hover:text-black text-black border-0 text-xl">
+              {
+                showAdminAttendanceSettings ?
+                <p className="bg-red-400 px-2 py-0.5 rounded-full text-white">Close</p>
+                :<i className="fas fa-cog"></i>
+              }
+
+            </Button>
+            <Overlay target={settingsTarget.current} show={showAdminAttendanceSettings} placement="bottom">
+              <Tooltip>
+                <div className="flex flex-col items-center">
+                  <p className="font-bold">Events Display Amount:</p>
+                  <Form.Control
+                  value={preDisplayEventsAmount}
+                  onChange={preDisplayEventAmountsHandler}
+                  type="text"
+                  placeholder=""
+                  className="w-1/2 text-center"
+                  />
+                  {showDisplayEventsAmountError ? <p className="text-red-400">Amount must be greater than 0 and less than or equal to {events.length}</p> : <div></div>}
+                </div>
+              </Tooltip>
+            </Overlay>
           </div>
           <div className="flex-grow"></div>
-          <button onClick={advanceEventIndicies}><i className="fas fa-chevron-right mr-5 bg-indigo-400 hover:bg-indigo-500 py-2.5 px-3 rounded-full text-white"></i></button>
+          <button onClick={advanceEventIndicies} disabled={endShowEventIndex === events.length}>
+            <i className={endShowEventIndex === events.length ? "fas fa-chevron-right mr-5 bg-indigo-300 py-2.5 px-3 rounded-full text-white" : "fas fa-chevron-right mr-5 bg-indigo-400 hover:bg-indigo-500 py-2.5 px-3 rounded-full text-white"}></i>
+          </button>
         </div>
        
         <Table striped bordered hover responsive>
